8bf56d2dd1ad5a6acb3a9743b957605cfc2279fb
Daniel Unapanta <dunapanta2@gmail.com>
2021-01-11 16:09:40 -0600

    Function to generate random number

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
-const GameScreen = () => {
+// funcion da un numero entre min y max
+const generateRandomBetween = (min, max, exclude) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
 
+    const rndNum = Math.floor((Math.random() * (max -min))) + min
+    // para que no adivine a la primera
+    if(rndNum === exclude){
+        return generateRandomBetween(min, max, exclude)
+    } else {
+        return rndNum
+    }
+}
+
+const GameScreen = ({ userChoice }) => {
+    const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, userChoice))
+    return(
+        <View>
+
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
