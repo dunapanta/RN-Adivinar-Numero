82f9eddac536cc67096279b3ab9d179d807ec0c3
Daniel Unapanta <dunapanta2@gmail.com>
2021-01-16 18:46:27 -0600

    Adding local image on GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -8,7 +8,13 @@ const GameOverScreen = ({ rounds, selectedNumber, newGame }) => {
         <View style={styles.screen}>
             <Text style={DefaultStyles.title}>Fin del Juego</Text>
             {/* For images you can use local  or network images*/}
-            <Image source={require('../assets/images/success.png')} />
+            <View style={styles.imageContainer}>
+                <Image 
+                    source={require('../assets/images/success.png')} 
+                    style={styles.image}
+                    resizeMode='cover'
+                />
+            </View>
             <Text style={DefaultStyles.bodyText}>Número de Rondas: {rounds}</Text>
             <Text style={DefaultStyles.bodyText}>El número elegido fue: {selectedNumber}</Text>
             <Button title="Volver a Jugar" onPress={newGame}/>
@@ -21,6 +27,19 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    imageContainer:{
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image:{
+        width: '100%',
+        height: '100%',
     }
 })
 
