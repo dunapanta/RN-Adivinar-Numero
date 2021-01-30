019d8a9af8e496901743d70d589af5073b116350
Daniel Unapanta <dunapanta2@gmail.com>
2021-01-30 14:33:36 -0600

    Adding SafeAreaView for respect the notch of devices and never overlaps

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
 import * as Font from 'expo-font'
 /* import { AppLoading } from 'expo' */
 import AppLoading from 'expo-app-loading'
@@ -55,10 +55,10 @@ export default function App() {
 
 
   return (
-    <View style={styles.screen}>
+    <SafeAreaView style={styles.screen}>
       <Header title="Adivinar el Número"/>
           {content}
-    </View>
+    </SafeAreaView>
   );
 }
 
